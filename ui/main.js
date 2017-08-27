1a9a433340d8d91144298d310192575b60d0df55
shubh199815 <shubh199815@gmail.com>
2017-08-27 15:17:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //Change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
+
+//Move the weirdo logo!!!
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft='100 px';
+};
