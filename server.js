f8c3168e027534d27c8e5d050b785acfabab492b
shubh199815 <shubh199815@gmail.com>
2017-08-27 08:20:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,107 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articles = {
+    
+    'article-one' : {
+        title:'Article One | Shubham Kumar',
+        heading: 'Article One',
+        date: 'Aug 27,2017',
+        content:`   
+                    <p>
+                        This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :) 
+                    </p>
+                    <p>
+                        This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :) 
+                    </p>
+                    <p>
+                        This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :)  This is the content of my first article :) 
+                    </p>`
+         },
+         
+    'article-two' : {
+        
+        title: 'Article Two | Shubham Kumar',
+        heading: 'Article One',
+        date: 'Aug 27,2017',
+        content:`   
+                    <p>
+                        This is the content of my second article :) 
+                    </p>`
+    },
+    
+    'article-three' : {
+        
+        title: 'Article Three | Shubham Kumar',
+        heading: 'Article Three',
+        date: 'Aug 27,2017',
+        content:`   
+                    <p>
+                        This is the content of my third article :)  
+
+                    </p>`
+        }
+        
+    
+};
+
+function createTemplate(data){
+    
+    var title = data.title;
+    var date= data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        
+        <body>
+            <div class="container">
+                <div>
+                    <a href='/'>HOME</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                   ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    
+    
+    
+    `;
+    return htmlTemplate;
+}
+
+
+
+
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname,'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName',function(req,res){
+    var articleNAme= req.params.articleNAme;
+    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
