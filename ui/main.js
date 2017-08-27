8c22f70a9595a421a488a6e48ba55fc33a2e3eb9
shubh199815 <shubh199815@gmail.com>
2017-08-27 16:11:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = 'New value';
 
 //Move the weirdo logo!!!
 var img = document.getElementById('madi');
+var marginLeft= 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveRight, 100);
 };
