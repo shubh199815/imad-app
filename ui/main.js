861687c5f9a8e09972def3ea1f1b3934eb55a7e9
shubh199815 <shubh199815@gmail.com>
2017-08-28 14:38:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //Change the text of main-text div
 var element = document.getElementById('main-text');
-element.innerHTML = 'New value';
+element.innerHTML = '';
 
 //Move the weirdo logo!!!
 var img = document.getElementById('madi');
