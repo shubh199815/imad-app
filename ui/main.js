13aa376b0f77d3b378d229e8e7b554e25ff8b082
shubh199815 <shubh199815@gmail.com>
2017-08-30 05:37:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,25 @@
-console.log('Loaded!');
+//Counter code
+var button= document.getElementById('counter');
 
-//Move the weirdo logo!!!
-var img = document.getElementById('madi');
-var marginLeft= 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+button.onclick = function () {
+    
+    //Create a request object
+    var request =  new XMLHttpRequest();
+    
+    //Capture the response and store it in variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE ){
+            //Take some action
+            if(request.status === 200){
+                var counter = request.responseText; 
+                var count = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    //Meke the request
+    request.open('GET','http://shubh199815.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
 };
