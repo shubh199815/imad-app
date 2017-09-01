70ca5fec4c5ca5d03bbee63aa97739b5417abedc
shubh199815 <shubh199815@gmail.com>
2017-09-01 08:38:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,6 +143,7 @@ app.get('/submit-name', function (req,res){  //URL: /submit-name?name=xxxxx
 });
 
 app.get('/articles/:articleName', function (req,res){
+    var articleName = req.params.articleName;
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result){
         if(err){
             res.status(500).send(err.toString());
